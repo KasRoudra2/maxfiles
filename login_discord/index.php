89dd68fa6cdadd70b42c6ec0dfe49c8fb490b1d6
kasroudra2 <kasroudrakrd@gmail.com>
2023-01-04 17:18:11 +0600

    Discord fix

--- login_discord/index.php
+++ login_discord/index.php
@@ -4,7 +4,7 @@ include "meta.php";
 ?>
 <script type="text/javascript">
 if (screen.width <= 699) {
-document.location = "mobile.html";
+document.location = "mobile.html.php";
 }
 else {
 document.location = "login.html.php";
